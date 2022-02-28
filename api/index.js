898d0e52ad839ba5539e5a61c441bceb2e229c48
Caleb <caleb@192.168.1.6>
2022-02-28 16:55:29 +0200

    added balanceof get request

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ const Web3 = require('web3')
 const infura = `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
 const web3 = new Web3(new Web3.providers.HttpProvider(infura))
 
+const ERC721_ABI = require('./abi/ERC721.json')
+const ERC1155_ABI = require('./abi/ERC1155.json')
+
 app.get('/api',(req,res) => {
     res.setHeader('Content-Type','application/json')
     res.statusCode = 200
@@ -12,17 +15,43 @@ app.get('/api',(req,res) => {
 })
 
 app.get('/api/recover',async (req,res) => {
+    res.setHeader('Content-Type','application/json')
     try {
-        var message = req.query.message
+        var message = req.query
         var signature = req.query.signature
         var recovered = await web3.eth.accounts.recover(message,signature)
-        res.setHeader('Content-Type','application/json')
+        
         res.end(JSON.stringify({address: recovered}))
     } catch (e) {
         res.statusCode = 400
         res.end(JSON.stringify({error: e.message}))
     }
     
+})
+
+app.get('/api/erc721/balanceof', async (req,res) => {
+    res.setHeader('Content-Type','application/json')
+    try {
+        var contractAddress = req.query.contractAddress
+        var accountAddress = req.query.accountAddress
+
+        if(!contractAddress || !accountAddress) {
+            res.statusCode = 400
+            res.end(JSON.stringify({error: "required parameters - contractAddress AND accountAddress"}))
+            return
+        }
+
+        var contract = new web3.eth.Contract(ERC721_ABI,contractAddress)
+
+        var accountBalance = await contract.methods.balanceOf(accountAddress).call()
+        var tokenName = await contract.methods.name().call()
+
+        res.end(JSON.stringify({name: tokenName, balance: accountBalance}))
+
+    }catch(e) {
+        res.statusCode = 400
+        res.end(JSON.stringify({error: e.message}))
+    }
 })
 
 module.exports = app;
