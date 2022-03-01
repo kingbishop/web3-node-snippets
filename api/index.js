35851151c4e66e7171a7b5559167181694951e49
Caleb <caleb@192.168.1.6>
2022-03-01 12:44:29 +0200

    Fixed issue with api recover

--- api/index.js
+++ api/index.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const app = require('express')()
 const Web3 = require('web3')
 
-const infura = `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
-const web3 = new Web3(new Web3.providers.HttpProvider(infura))
+const rinkeby = `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+const web3 = new Web3(new Web3.providers.HttpProvider(rinkeby))
 
 const ERC721_ABI = require('./abi/ERC721.json')
 const ERC1155_ABI = require('./abi/ERC1155.json')
@@ -17,8 +17,9 @@ app.get('/api',(req,res) => {
 app.get('/api/recover',async (req,res) => {
     res.setHeader('Content-Type','application/json')
     try {
-        var message = req.query
+        var message = req.query.message
         var signature = req.query.signature
+        
         var recovered = await web3.eth.accounts.recover(message,signature)
         
         res.end(JSON.stringify({address: recovered}))
